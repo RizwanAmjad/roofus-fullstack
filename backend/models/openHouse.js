0b366acffe3e2abf2a85f6d2fdfaab8e6faccbfb
RizwanAmjad <rizwanamjadms@gmail.com>
2023-01-24 14:59:05 +0500

    Created the openHouse mongoose and joi schema

---
+++ backend/models/openHouse.js
@@ -0,0 +1,27 @@
+const Joi = require("joi")
+const mongoose = require("mongoose")
+
+const OpenHouse = mongoose.model(
+  "OpenHouse",
+  mongoose.Schema({
+    property: { type: mongoose.Schema.Types.ObjectId, ref: "Property" },
+    visitorAmount: { type: Number, required: true, min: 0 },
+    startDate: { type: Number, required: true },
+    endDate: { type: Number, required: true },
+  })
+)
+
+const validateOpenHouseJoi = (openHouse) => {
+  const schema = Joi.object({
+    propertyId: Joi.string()
+      .regex(/^[0-9a-fA-F]{24}$/)
+      .message("Property Id must a valid Object Id"),
+    visitorAmount: Joi.number().required().min(0).label("Visitor Amount"),
+    startDate: Joi.number().required().min(0).label("Start Date"),
+    endDate: Joi.string().required().label("End Date"),
+  })
+  const { error } = schema.validate(openHouse)
+  return error ? error.details[0].message : error
+}
+
+module.exports = { OpenHouse, validateOpenHouseJoi }
